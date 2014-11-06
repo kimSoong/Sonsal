914d4539dd908fa75de85536fe061979ef1a6116
KimSoong <kimsoong@KimSoongui-MacBook-Pro.local>
2014-11-06 20:23:36 +0900

    Initial Commit

---
+++ Sunsal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sunsal
+//
+//  Created by KimSoong on 2014. 11. 6..
+//  Copyright (c) 2014년 kims1006. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
